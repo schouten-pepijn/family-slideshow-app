5775aa3abc75fbdba1493912ef63b6be7b18898b
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-13 23:34:20 +0100

    initialized tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
+    host: true,
     port: 5173,
-    strictPort: true
-  }
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
