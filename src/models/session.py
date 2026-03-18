600e2412960fe566418b8c36088b5a3800ad8272
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-18 23:39:56 +0100

    feat: add Session model (sessions)

---
+++ src/models/session.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id: Mapped[int] = mapped_column(
+        String(64),
+        primary_key=True,
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False,
+    )
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.now(),
+    )
