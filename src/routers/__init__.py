dac3e82097427d152cd99bd7b7ea9efbd11f6b6e
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-19 01:35:14 +0100

    refactor: expose photos router (routers)

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,3 +1,7 @@
 from src.routers.auth import router as auth_router
+from src.routers.photos import router as photos_router
 
-__all__ = ["auth_router"]
+__all__ = [
+    "auth_router",
+    "photos_router",
+]
