9e58a2221c73a898d3235ef118162d5117c73c51
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-19 21:38:57 +0100

    feat: expose Collection & junction models (api)

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,13 @@
+from src.models.collection import Collection
 from src.models.photo import Photo
+from src.models.photo_collection_junction import PhotoCollectionJunction
 from src.models.session import Session
 from src.models.user import User
 
 __all__ = [
+    "Collection",
     "Photo",
+    "PhotoCollectionJunction",
     "Session",
     "User",
 ]
