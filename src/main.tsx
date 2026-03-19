33d1155d960b1914d4d3c1387238da823e8872e5
Pepijn Schouten <pepijn.sch@gmail.com>
2026-03-19 13:14:27 +0100

    feat: add AuthProvider wrapper (auth)
    Wrap the application in AuthProvider to provide authentication context to child components.

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./hooks/useAuth";
 import { PhotosProvider } from "./hooks/usePhotos";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <PhotosProvider>
-        <App />
-      </PhotosProvider>
+      <AuthProvider>
+        <PhotosProvider>
+          <App />
+        </PhotosProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
